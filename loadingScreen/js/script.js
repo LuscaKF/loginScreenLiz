7d2c1400a5cf46a41e26fcfafd8513c0296e2a26
Lucas Kirow Fernandes <62342102+LuscaKF@users.noreply.github.com>
2024-11-04 17:16:47 -0300

    Adicionando páginas de login de redefinição de senha.

--- loadingScreen/js/script.js
+++ loadingScreen/js/script.js
@@ -1,9 +1,6 @@
-// script.js
 document.addEventListener("DOMContentLoaded", () => {
-    // Define o tempo de espera na tela de carregamento (em milissegundos)
     const loadingTime = 3000; // 3 segundos
 
-    // Função para redirecionar para a tela de login
     setTimeout(() => {
         window.location.href = "login.html";
     }, loadingTime);
